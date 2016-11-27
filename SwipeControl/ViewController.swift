b3c52f50465b447f5f8495a7d45a4ab436767f44
Oni_01 <oni.zerone@gmail.com>
2016-11-27 19:13:01 +0100

    SwipeControl chore: remove unsesiderable comments.

--- SwipeControl/ViewController.swift
+++ SwipeControl/ViewController.swift
@@ -42,7 +42,7 @@ class ViewController: UIViewController, SOTSwipeControlDelegate {
         }));
         
         self.present(alert, animated: true) {
-            //POBA!
+            //DO NOTHING.
         };
         
     }
